2e99b42766c5b3769bc5128e2b5930c5d159cb24
robkemp <rjameskemp@gmail.com>
2016-05-20 15:19:01 -0600

    fixed reactivity in modules

--- R/county_components.R
+++ R/county_components.R
@@ -11,7 +11,7 @@
 
 county_components = function(input, output, session, fips=0, name="Colorado"){
 
-  data=reactive({county_profile(fips,c(1985:2014), vars=c("naturalincrease", "netmigration"))})
+  data=reactive({county_profile(fips(),c(1985:2014), vars=c("naturalincrease", "netmigration"))})
 
 
   output$components_p=renderPlotly({plot_ly(data(), x=year,y=as.numeric(naturalincrease)+as.numeric(netmigration), line=list(color="rgb(31,74,126)", width=2.5, dash="solid"), name= "Total Population Change")%>%
@@ -19,7 +19,7 @@ county_components = function(input, output, session, fips=0, name="Colorado"){
       add_trace(x=year,y=as.numeric(netmigration), type= "line", line=list(color = "rgb(92,102,112)", width=2.5, dash="dot"), name="Net Migration")%>%
       layout(
         barmode="stacked",
-        title=paste(name, " Births, Deaths, and Net Migration 1985 to", as.character(max(data()$year))),
+        title=paste(name(), "Births, Deaths, and Net Migration 1985 to", as.character(max(data()$year))),
         xaxis=list(
           title="Year"),
         yaxis=list(
